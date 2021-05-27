401d41dde1401b346a24501f27ef75924169986d
zerckel <antoinecarbonnel@yahoo.fr>
2021-05-27 16:03:49 +0200

    update

--- app/src/main/java/MainActivity.java
+++
@@ -1,8 +0,0 @@
-/**
- *
- */
-public class MainActivity {
-    public MainActivity() {
-
-    }
-}
---
+++ app/src/main/java/a2048/MainActivity.java
@@ -0,0 +1,23 @@
+package a2048;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a2048.R;
+
+/**
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public MainActivity() {
+
+    }
+}
