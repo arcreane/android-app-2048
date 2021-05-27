f3a4982021eb6732fafd01b9df4f94fdbeb9887f
zerckel <antoinecarbonnel@yahoo.fr>
2021-05-27 15:25:13 +0200

    Update class init each variable and methods

--- app/src/main/java/Case.java
+++ app/src/main/java/Case.java
@@ -1,2 +1,88 @@
+/**
+ *
+ */
 public class Case {
+    private int Value;
+    private int PosX;
+    private int PosY;
+
+    /**
+     * @param value Value of the case
+     * @param posX Localisation of the case in X axis
+     * @param posY Localisation of the case in Y axis
+     */
+    public Case(int value, int posX, int posY) {
+        Value = value;
+        PosX = posX;
+        PosY = posY;
+    }
+
+    /**
+     *
+     */
+    public void InitCase() {
+
+    }
+
+    /**
+     *
+     */
+    public void Fusion() {
+
+    }
+
+    /**
+     *
+     */
+    public void Move() {
+    }
+
+    /**
+     *
+     */
+    public void FusionCase() {
+
+    }
+
+    /**
+     * @return Value
+     */
+    public int getValue() {
+        return Value;
+    }
+
+    /**
+     * @param value Set value of the case
+     */
+    public void setValue(int value) {
+        Value = value;
+    }
+
+    /**
+     * @return PosX of the case
+     */
+    public int getPosX() {
+        return PosX;
+    }
+
+    /**
+     * @param posX Set X axis of the case
+     */
+    public void setPosX(int posX) {
+        PosX = posX;
+    }
+
+    /**
+     * @return PosY of the case
+     */
+    public int getPosY() {
+        return PosY;
+    }
+
+    /**
+     * @param posY Set Y axis of the case
+     */
+    public void setPosY(int posY) {
+        PosY = posY;
+    }
 }
--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,2 +1,51 @@
+/**
+ *
+ */
 public class User {
+    private String Pseudo;
+    private int[] HighScore;
+
+    /**
+     * @param HighScore ranking the best score of the user
+     */
+    public User(int[] HighScore) {
+
+    }
+
+    public void DisplayHighScore() {
+    }
+
+    public void SendScore() {
+    }
+
+    public void OrderHighScore() {
+    }
+
+    /**
+     * @return String
+     */
+    public String getPseudo() {
+        return Pseudo;
+    }
+
+    /**
+     * @param pseudo set pseudo of the user
+     */
+    public void setPseudo(String pseudo) {
+        Pseudo = pseudo;
+    }
+
+    /**
+     * @return HighScore
+     */
+    public int[] getHighScore() {
+        return HighScore;
+    }
+
+    /**
+     * @param highScore set HighScore of the actual user
+     */
+    public void setHighScore(int[] highScore) {
+        HighScore = highScore;
+    }
 }
