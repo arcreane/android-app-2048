1ef27258a523219d4231e45481060a8ac459de13
zerckel <antoinecarbonnel@yahoo.fr>
2021-06-19 22:48:55 +0200

    resolve crash error and reverted loop to prompt score in good order

--- app/src/main/java/a2048/HighScoreDialogFragment.java
+++ app/src/main/java/a2048/HighScoreDialogFragment.java
@@ -26,7 +26,7 @@ public class HighScoreDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(this.HighScoreTableBuilder().equals("") ? "No Score register yet :(" : this.HighScoreTableBuilder())
+        builder.setMessage(this.HighScoreTableBuilder().equals("Score: \n") ? "No Score register yet :(" : this.HighScoreTableBuilder())
                 .setPositiveButton("Wow, this is incredible!", (dialog, id) -> {
                 });
         return builder.create();
@@ -34,8 +34,9 @@ public class HighScoreDialogFragment extends DialogFragment {
 
     private String HighScoreTableBuilder() {
         StringBuilder HighScoreTable = new StringBuilder();
-        for (int i : this.getHighScore()) {
-            HighScoreTable.append(getHighScore()[i]);
+        HighScoreTable.append("Score: \n");
+        for (int i = this.getHighScore().length; i > 0; i--) {
+            HighScoreTable.append(this.getHighScore()[i - 1]).append("\n");
         }
         return HighScoreTable.toString();
     }
